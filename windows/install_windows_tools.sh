be593b3c085d31e5d44d1e5c8b61336f71551d75
Genaro <fabian.huamani@estudiante.ucsm.edu.pe>
2025-09-01 16:32:39 -0500

    Update install_windows_tools.sh

--- windows/install_windows_tools.sh
+++ windows/install_windows_tools.sh
@@ -4,6 +4,7 @@ cd /tools/windows/
 tools=(
   "https://raw.githubusercontent.com/peass-ng/PEASS-ng/master/winPEAS/winPEASps1/winPEAS.ps1 winPEAS.ps1"
   "https://github.com/peass-ng/PEASS-ng/releases/latest/download/winPEASany_ofs.exe winPEASany_ofs.exe"
+  "https://raw.githubusercontent.com/lukebaggett/dnscat2-powershell/refs/heads/master/dnscat2.ps1 dnscat2.ps1"
 )
 
 # Descargar cada herramienta
